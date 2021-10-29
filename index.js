63c8ab8c42fee01de75aa29965d34132387f3fc5
David8393 <jo974586david@gmail.com>
2021-10-29 11:51:26 +0500

    new homework

--- index.js
+++ index.js
@@ -1,122 +1,123 @@
 
 // Math pow
 
-function pow(a, b) {
-    return a ** b; 
- }
+// function pow(a, b) {
+//     return a ** b; 
+//  }
 
-console.log(pow(5, 3));
+// console.log(pow(5, 3));
 
 // Math sqrt
 
-function sqrt(a) {
-    return a ** 0.5;
-}
+// function sqrt(a) {
+//     return a ** 0.5;
+// }
 
-console.log(sqrt(36));
+// console.log(sqrt(36));
 
 // Math cbrt
 
-function cbrt(a) {
-   return (a ** (1/3));
-}
+// function cbrt(a) {
+//    return (a ** (1/3));
+// }
 
- console.log(cbrt(8));
+//  console.log(cbrt(8));
 
 // Math abs
 
-function abs(a) {
-    return (a  ** 2) ** 0.5;
-}
+// function abs(a) {
+//     return (a  ** 2) ** 0.5;
+// }
 
-console.log(abs(-20));
+// console.log(abs(-20));
 
 // Math round
 
-function round(a) {
-   return (a).toFixed(0);
-}
+// function round(a) {
+//    return (a).toFixed(0);
+// }
 
-console.log(round(10.1))
+// console.log(round(10.1))
 
 // Math floor
 
-function floor(a) {
-    return parseInt(a);
-}
+// function floor(a) {
+//     return parseInt(a);
+// }
 
-console.log(floor(20.9))
+// console.log(floor(20.9))
 
 // Math ceil
 
-function ceil(a) {
-    return (a + 0.5).toFixed(0);
-}
+// function ceil(a) {
+//     return (a + 0.5).toFixed(0);
+// }
 
-console.log(ceil(4.02));
+// console.log(ceil(4.02));
 
 // New task
 
 // 0 ~ 100 => toq, juft, 
-for (let i = 1; i <= 100; i = i + 2) {
-    console.log(i);
-}
+// for (let i = 1; i <= 100; i = i + 2) {
+//     console.log(i);
+// }
 
-for (let i = 0; i <= 100; i = i + 2) {
-    console.log(i);
-}
+// for (let i = 0; i <= 100; i = i + 2) {
+//     console.log(i);
+// }
 // 10 ~ 0
-for (let i = 10; i >= 0; i --) {
-    console.log(i)
-}
+// for (let i = 10; i >= 0; i --) {
+//     console.log(i)
+// }
 // title = "webBrain Academy" x 5
-let A = 5;
-let B = "Webbrain academy ";
-let Str = '';
+// let A = 5;
+// let B = "Webbrain academy ";
+// let Str = '';
 
-for (let i = 0; i < A; i++) {
-    Str = Str + B
-}
-console.log(Str);
+// for (let i = 0; i < A; i++) {
+//     Str = Str + B
+// }
+// console.log(Str);
 // a, b
 // a
 
-var txt = "welcome webbrain ";
-var rep = txt.repeat(5);
-count = 0;
+// var txt = "welcome webbrain ";
+// var rep = txt.repeat(5);
+// count = 0;
 
-for (var i = 0; i < rep.length; i++) {
-    if (rep.charAt(i) === 'a') {
-        count++
-    }
-}
+// for (var i = 0; i < rep.length; i++) {
+//     if (rep.charAt(i) === 'a') {
+//         count++
+//     }
+// }
 
-console.log(count);
+// console.log(count);
 
 // b
 
-var txt = 'welcome webbrain ';
-var rep = txt.repeat(5);
-count = 0;
+// var txt = 'welcome webbrain ';
+// var rep = txt.repeat(5);
+// count = 0;
 
-for (var i = 0; i < rep.length; i++) {
-  if (rep.charAt(i) === 'b') {
-    count++;
-  }
-}
+// for (var i = 0; i < rep.length; i++) {
+//   if (rep.charAt(i) === 'b') {
+//     count++;
+//   }
+// }
 
-console.log(count);
+// console.log(count);
+
 //remove white space
 
-let A = 5;
-let B = "Webbrain academy";
-let Str = '';
+// let A = 5;
+// let B = "Webbrain academy";
+// let Str = '';
 
-for (let i = 0; i < A; i++) {
-   Str += B.trim();
-}
+// for (let i = 0; i < A; i++) {
+//    Str += B.trim();
+// }
 
-console.log(Str);
+// console.log(Str);
 
 
 
