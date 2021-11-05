0da5ca06a1f05e42c13eab0db5a9343e82bfcd3e
David8393 <jo974586david@gmail.com>
2021-11-05 14:18:35 +0500

    test

---
+++ array/array.js
@@ -0,0 +1,46 @@
+// const array = [45, "salom", undefined, true,"oooo",92398]
+// console.log(array[6])
+// array.push("salommama")
+// array.unshift("qales")
+// array.shift("")
+// array.pop()
+// let array2 = ["nima", 321513, 12399, "oppooo"]
+// let array3 = [array.concat(array2)]
+// console.log(array3);
+// array.slice(1, 2);
+// console.log(array);
+// console.log(array.splice(1, 3));
+// console.log(array);
+
+// splice
+// ctrl + x
+
+
+// salom;
+
+
+// slice 
+// ctrl + c
+// salom
+
+// salom;
+
+// vazifa 
+
+// var fruits = ['apple', 12, 'Ananas', 'Orange', 'Banana', 'Kiwi'];
+// fruits = fruits.toString();
+// let result1 = 0;
+// let result2 = 0;
+
+// for (let i = 0; i < fruits.length; i++) {
+//     const element = fruits[i];
+
+//     for (let j = 0; j < i.length; j++) {
+//         const element = i[j];
+        
+//     }
+    
+// }
+// console.log(fruits)
+
+// var number = [10, 5, 11, 'apple', 2, 3, 4, 5, 6];
