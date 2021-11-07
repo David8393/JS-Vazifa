ecd2d0c940c44889cef46f1eb741fb4ad78deece
David8393 <jo974586david@gmail.com>
2021-11-08 02:01:37 +0500

    test

---
+++ array object homework/index.js
@@ -0,0 +1,29 @@
+ar[
+    {
+        id: 1,
+        year: 2010,
+        name: 'Eshmat',
+        status: 'student',
+        key: 'test',
+    },
+    {
+        id: 2,
+        year: 2011,
+        name: 'Toshmat',
+        status: 'student',
+        key: 'test',
+    },
+    {
+        id: 3,
+        year: 2012,
+        name: 'Eshmatning akasi',
+        status: 'student',
+        key: 'test',
+    }
+];
+
+console.log(user);
+
+const onDelete = (id) => { }
+
+const onSort = (value, key) => {}
