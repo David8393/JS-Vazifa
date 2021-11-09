c49f9ebc7212c3c898061e7e48b5ff8f40fa3e0b
David8393 <jo974586david@gmail.com>
2021-11-09 09:50:44 +0500

    test

--- object/index.js
+++ object/index.js
@@ -1,38 +1,50 @@
 users = [
-    {
-        id: 1,
-        year: 2010,
-        name: 'Eshmat',
-        status: 'student',
-        key: 'test',
-    },
-    {
-        id: 2,
-        year: 2011,
-        name: 'Toshmat',
-        status: 'student',
-        key: 'test',
-    },
-    {
-        id: 3,
-        year: 2012,
-        name: 'Eshmatning akasi',
-        status: 'student',
-        key: 'test',
-    }
+  {
+    id: 1,
+    year: 2010,
+    name: 'Eshmat',
+    status: 'student',
+    key: 'test',
+  },
+  {
+    id: 2,
+    year: 2011,
+    name: 'Toshmat',
+    status: 'student',
+    key: 'test',
+  },
+  {
+    id: 3,
+    year: 2012,
+    name: 'Eshmatning akasi',
+    status: 'student',
+    key: 'test',
+  },
 ];
-
-// const onSort = (value, key) => { };
 
 // onDelete //
 
-const onDelete = (users, id) => {
-  let data = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i]['id'] !== id) {
-      data.push(users[i]);
-    }
-  }
-  return data;
-};
-console.log(onDelete(users, 1));
+// const onDelete = (users, id) => {
+//   let data = [];
+//   for (let i = 0; i < users.length; i++) {
+//     if (users[i]['id'] !== id) {
+//       data.push(users[i]);
+//     }
+//   }
+//   return data;
+// };
+// console.log(onDelete(users, 1));
+
+// const onSort = (value, key) => { };
+
+//sortedByName
+
+// const a = users.filter((value) => value.name === 'Eshmat') 
+
+// console.log(a);
+
+// sortedByStatus //
+
+// const a = users.filter((value) => value.status === 'student');
+
+// console.log(a);
