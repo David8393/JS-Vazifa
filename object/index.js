17c34affda37b0d741d5f402797dd7607544d3f1
David8393 <jo974586david@gmail.com>
2021-11-10 13:03:08 +0500

    test

--- object/index.js
+++ object/index.js
@@ -1,26 +1,26 @@
-users = [
-  {
-    id: 1,
-    year: 2010,
-    name: 'Eshmat',
-    status: 'student',
-    key: 'test',
-  },
-  {
-    id: 2,
-    year: 2011,
-    name: 'Toshmat',
-    status: 'student',
-    key: 'test',
-  },
-  {
-    id: 3,
-    year: 2012,
-    name: 'Eshmatning akasi',
-    status: 'student',
-    key: 'test',
-  },
-];
+// users = [
+//   {
+//     id: 1,
+//     year: 2010,
+//     name: 'Eshmat',
+//     status: 'student',
+//     key: 'test',
+//   },
+//   {
+//     id: 2,
+//     year: 2011,
+//     name: 'Toshmat',
+//     status: 'student',
+//     key: 'test',
+//   },
+//   {
+//     id: 3,
+//     year: 2012,
+//     name: 'Eshmatning akasi',
+//     status: 'student',
+//     key: 'test',
+//   },
+// ];
 
 // onDelete //
 
