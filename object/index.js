bef4680c1e03cc3298681f6cf7b9ef18932657f3
David8393 <jo974586david@gmail.com>
2021-11-10 12:50:11 +0500

    test

--- object/index.js
+++ object/index.js
@@ -48,3 +48,39 @@ users = [
 // const a = users.filter((value) => value.status === 'student');
 
 // console.log(a);
+
+// 1. Object kalitlari va qiymatlarini tengligini tekshirish
+// const data = { a: 1, b: 1 };
+// const data2 = { a: 1, b: 1 };
+// const data3 = { a: 1, b: 2 };
+
+// areEqual( data, data2 )  => true
+// areEqual( data, data3 )  => false
+
+// const areEqual = (value1, value2) => {
+//   if (Object.values(value1).toString() === Object.values(value2).toString()) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// };
+// areEqual(data, data2);
+
+// 2. Object ichidagi Objectni qiymatlari mavjudligini tekshirish
+
+// const data = { a: { b: undefined } };
+// const data2 = { a: { b: 'it does exits' } };
+// const data3 = { a: { b: 'it does exits', innerKey: null } };
+
+// nestedValueExists(data)  => false
+// nestedValueExists(data2)  => true
+// nestedValueExists(data3)  => false
+
+// 3. Object qiymatlari va kalitlari bir xillari borligini tekshirish
+
+// const data = { a: 1, b: 2 };
+// const data2 = { c: 1, b: 2 };
+// const data3 = { c: 3, b: 5 };
+
+// checkSame( data, data2 )  => true
+// checkSame( data2, data3 )  => false
